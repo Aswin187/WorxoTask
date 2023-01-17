ef3093272877f0ccb8e9d5c8d39ae7cf57ad1c45
hafizmp <hafiznjh007@gmail.com>
2023-01-17 15:39:41 +0530

    task

--- src/Components/Banner2/BannerTwo.js
+++ src/Components/Banner2/BannerTwo.js
@@ -7,13 +7,13 @@ const BannerTwo = () => {
   return (
     <div>
       <Grid className= "banner" container style={{ alignItems: 'center' }}>
-        <img src='bannerTwo/Bannertwoimg.png' alt='two' style={{width:"100%"}}/>
+        <img src='https://raw.githubusercontent.com/Aswin187/WorxoTask/main/public/BannerImages/Bannertwoimg.png' alt='two' style={{width:"100%"}}/>
         <Grid item xs={8}  >
-          <img   src='bannerTwo/8OHYcD4EA5hpJqQgtQmNVfLrO0YXspuEDfseTmXX.png'  alt="" style={{width:"90%", height:"400px",marginLeft:'80px'}}/>
+          <img   src='https://raw.githubusercontent.com/Aswin187/WorxoTask/main/public/BannerImages/8OHYcD4EA5hpJqQgtQmNVfLrO0YXspuEDfseTmXX.png'  alt="" style={{width:"90%", height:"400px",marginLeft:'80px'}}/>
 
         </Grid>
         <Grid item xs={4}>
-          <img   className='banner2' src='bannerTwo/M2dhebuxJ100xVTDB0sKwbvlQYyG9POXRyEYz9rk.png'  alt=""style={{width:"80%", height:"400px"}}/>
+          <img   className='banner2' src='https://raw.githubusercontent.com/Aswin187/WorxoTask/main/public/BannerImages/M2dhebuxJ100xVTDB0sKwbvlQYyG9POXRyEYz9rk.png'  alt=""style={{width:"80%", height:"400px"}}/>
 
         </Grid>
       </Grid>
